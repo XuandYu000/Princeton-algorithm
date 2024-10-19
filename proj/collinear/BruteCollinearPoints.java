ce8ebb0b221820f6a1624212bcde2d23c32d5b79
XuandYu000 <xuzhiyu2022@gmail.com>
2024-10-19 22:43:49 +0800

    The BruteCollinearPoint has some Corner cases I didn't find. Fixed

--- proj/collinear/BruteCollinearPoints.java
+++ proj/collinear/BruteCollinearPoints.java
@@ -1,66 +1,78 @@
+/* *****************************************************************************
+ *  Name:
+ *  Date:
+ *  Description:
+ **************************************************************************** */
+
 import edu.princeton.cs.algs4.In;
 import edu.princeton.cs.algs4.StdDraw;
 import edu.princeton.cs.algs4.StdOut;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 
 public class BruteCollinearPoints {
-    private static final int POINTSONLINE = 3;
-    private Point[] points;
+    private List<LineSegment> collinearLineSegments = new ArrayList<>();
 
     // finds all line segments containing 4 points
     public BruteCollinearPoints(Point[] points) {
+        // corner case checking
         if (points == null) {
-            throw new IllegalArgumentException("The given points is null");
+            throw new IllegalArgumentException("argument to constructor is null");
         }
-        this.points = points;
-        Arrays.sort(points);
+        for (Point p : points) {
+            if (p == null) {
+                throw new IllegalArgumentException("one point is null");
+            }
+        }
+        int len = points.length;
+        for (int i = 0; i < len; i++) {
+            for (int j = i + 1; j < len; j++) {
+                if (points[i].compareTo(points[j]) == 0) {
+                    throw new IllegalArgumentException("repeated point");
+                }
+            }
+        }
+        if (len < 4) {
+            return;
+        }
+
+        Point[] tmp = Arrays.copyOf(points, len);
+        Arrays.sort(tmp);
+        // brute check
+        for (int i = 0; i < len; i++) {
+            for (int j = i + 1; j < len; j++) {
+                for (int k = j + 1; k < len; k++) {
+                    for (int h = k + 1; h < len; h++) {
+                        double s1 = tmp[i].slopeTo(tmp[j]);
+                        double s2 = tmp[i].slopeTo(tmp[k]);
+                        double s3 = tmp[i].slopeTo(tmp[h]);
+                        if (s1 == s2 && s1 == s3) {
+                            collinearLineSegments.add(new LineSegment(tmp[i], tmp[h]));
+                        }
+                    }
+                }
+            }
+        }
     }
 
     // the number of line segments
     public int numberOfSegments() {
-        int n = 0;
-        for (int i = 0; i < points.length - POINTSONLINE; i++) {
-            for (int j = i + 1; j < points.length - POINTSONLINE + 1; j++) {
-                for (int k = j + 1; k < points.length - POINTSONLINE + 2; k++) {
-                    for (int r = k + 1; r < points.length - POINTSONLINE + 3; r++) {
-                        double slope1 = points[i].slopeTo(points[j]);
-                        double slope2 = points[i].slopeTo(points[k]);
-                        double slope3 = points[i].slopeTo(points[r]);
-                        if (slope1 == slope2 && slope2 == slope3) {
-                            n++;
-                        }
-                    }
-                }
-            }
-        }
-        return n;
+        return collinearLineSegments.size();
     }
 
     // the line segments
     public LineSegment[] segments() {
-        int n = numberOfSegments();
-        int cnt = 0;
-        LineSegment[] segments = new LineSegment[n];
-        for (int i = 0; i < points.length - POINTSONLINE; i++) {
-            for (int j = i + 1; j < points.length - POINTSONLINE + 1; j++) {
-                for (int k = j + 1; k < points.length - POINTSONLINE + 2; k++) {
-                    for (int r = k + 1; r < points.length - POINTSONLINE + 3; r++) {
-                        double slope1 = points[i].slopeTo(points[j]);
-                        double slope2 = points[i].slopeTo(points[k]);
-                        double slope3 = points[i].slopeTo(points[r]);
-                        if (slope1 == slope2 && slope2 == slope3) {
-                            segments[cnt++] = new LineSegment(points[i], points[r]);
-                        }
-                    }
-                }
-            }
+        LineSegment[] retValue = new LineSegment[numberOfSegments()];
+        int i = 0;
+        for (LineSegment x : collinearLineSegments) {
+            retValue[i++] = x;
         }
-        return segments;
+        return retValue;
     }
 
     public static void main(String[] args) {
-
         // read the n points from a file
         In in = new In(args[0]);
         int n = in.readInt();
